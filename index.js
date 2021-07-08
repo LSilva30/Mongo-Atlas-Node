f750dd18318b560bf994149a1c8e6ecd95c4dcd6
luizsilva <luizfsilva.091595@gmail.com>
2021-07-08 15:20:20 -0400

    added .env, connected to mongo, updated git ignore, tested ok

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-
+require('dotenv/config')
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://Luiz:Luiz1995@cluster0.hafxr.mongodb.net/test', { useNewUrlParser: true })
+    .connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
     .then(() => console.log('We are connected to Mongo...'))
     .catch(err => console.log('Could not connect to MongoDB', err))
-    
+
+const movieSchema = mongoose.Schema({
+    title: String,
+    genre: [String],
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+function getAllMovies() {
+    Movie.find()
+        .then(allMovies => console.log('here are all the movies', allMovies))
+        .catch(err => console.log('could not get all movies', err))
+
+}
+
+getAllMovies()
